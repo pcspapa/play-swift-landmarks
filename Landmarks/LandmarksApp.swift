e1dac931e013ed2460349c7295556fa6f1c57c97
Chanseok Park <pcspapa@inzido.co.kr>
2021-06-09 15:26:38 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by make some noise on 2021/06/09.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
